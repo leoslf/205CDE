f06bb210697d5d79ca070cc2ba42eb591c266f6e
Leo Sin <leo69219362@gmail.com>
2018-03-01 14:47:22 +0800

    update

--- main.py
+++ main.py
@@ -1,26 +1,55 @@
 #!/usr/bin/env python
 
+import sys
+import os
 from flask import *
 from config import DevelopmentConfig
+from db_connection import *
+
+# helper functions
+def rootpath(path=""):
+    return os.path.dirname(os.path.abspath(__file__)) + "/" + path
 
 # instantiate a Flask object
-app = Flask(__name__)
+#app = Flask(__name__, template_folder=rootpath('templates'), static_url_path="/app/static")
+app = Flask(__name__, template_folder=rootpath('templates'))
 app.config.from_object(DevelopmentConfig)
+app.secret_key = 'KEY'
 
 # matching route and handler
-@app.route('/success/<name>')
-def success(name):
-    return "Welcome %s" % name
+@app.route('/')
+def root():
+    return loadfile('index.html')
+
+@app.route('/<filename>', methods = ['GET', 'POST'])
+def loadfile(filename):
+    return app.send_static_file(filename)
 
 @app.route("/login", methods = ['POST', 'GET'])
 def login():
-    if request.method == 'POST':
-        user = request.form['nm']
-    else:
-        user = request.args.get('nm')
-    if user is None:
-        user = "__None__"
-    return redirect(url_for('success', name = user))
+    content = ""
+    # Login Already 
+    if 'user' in session and 'displayed_username' in session:
+        return redirect(url_for('root'))
+
+    if request.method == "POST":
+        # login form submitted
+        result = None
+        try:
+            result = query("staff", "CONCAT(firstname, ' ', lastname) AS name", "username='%s' AND password='%s'" % (request.form['username'], request.form['password']))
+        except Exception as e:
+            return "Exception: " + str(e)
+
+        # login successful
+        if len(result["rows"]) == 1: 
+            session['user'] = request.form['username']
+            session['displayed_username'] = result["rows"][0]['name']
+            return redirect(request.referrer)
+        print (result)
+        return "Login Error, returned number of rows != 1"
+
+    # before login
+    return render_template('login.html')
 
 # prevent execution when this module is imported by others
 if __name__ == "__main__":

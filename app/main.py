3eb8c841e55f809d8e09f7efa051e0b9763f9817
Leo Sin <leo69219362@gmail.com>
2018-03-02 23:04:42 +0800

    change quotation to double quotation in main.py; fix indentation in base.html; create admin.html frame

--- app/main.py
+++ app/main.py
@@ -11,57 +11,61 @@ def rootpath(path=""):
     return os.path.dirname(os.path.abspath(__file__)) + "/" + path
 
 # instantiate a Flask object
-app = Flask(__name__, template_folder=rootpath('templates'))
+app = Flask(__name__, template_folder=rootpath("templates"))
 app.config.from_object(DevelopmentConfig)
-app.secret_key = 'KEY'
+app.secret_key = "KEY"
 
 @app.before_request
 def before_request():
     g.nav = query("navigation")
 
 # matching route and handler
-@app.route('/')
-def root():
-    return render_template('index.html')
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/<filename>', methods = ['GET', 'POST'])
+@app.route("")
+def admin():
+    return render_template("admin.html")
+
+@app.route("/<filename>", methods = ["GET", "POST"])
 def send_static(filename):
     return app.send_static_file(filename)
 
-@app.route("/login", methods = ['POST', 'GET'])
+@app.route("/login", methods = ["POST", "GET"])
 def login():
     content = ""
     # Login Already 
-    if 'user' in session and 'displayed_username' in session:
-        return redirect(url_for('root'))
+    if "user" in session and "displayed_username" in session:
+        return redirect(url_for("root"))
 
     if request.method == "POST":
         # login form submitted
         result = None
         try:
-            results = query("staff", "CONCAT(firstname, ' ', lastname) AS name", "username='%s' AND password='%s'" % (request.form['username'], request.form['password']))
+            results = query("staff", "CONCAT(firstname, " ", lastname) AS name", "username="%s" AND password="%s"" % (request.form["username"], request.form["password"]))
         except Exception as e:
             return "Exception: " + str(e)
 
         # login successful
         if len(results) == 1: 
-            session['user'] = request.form['username']
-            session['displayed_username'] = results[0]['name']
+            session["user"] = request.form["username"]
+            session["displayed_username"] = results[0]["name"]
             return redirect(request.referrer)
         print (results)
         if len(results) > 1:
             errmsg = "Login Error, returned number of rows > 1"
         errmsg = "Invalid username or password"
-        resp = make_response(render_template('login.html'))
-        resp.set_cookie('errmsg', errmsg)
+        resp = make_response(render_template("login.html"))
+        resp.set_cookie("errmsg", errmsg)
         return resp
     # before login
-    return render_template('login.html')
+    return render_template("login.html")
 
 @app.route("/logout")
 def logout():
-    session.pop('user', None)
-    session.pop('displayed_username', None)
+    session.pop("user", None)
+    session.pop("displayed_username", None)
     return redirect(request.referrer)
 
 # prevent execution when this module is imported by others

63d9c1c34ee07624d08f2f49f2d1ef069cb7c797
Leo Sin <leo69219362@gmail.com>
2018-03-12 15:44:24 +0800

    added setup sql from mysqldump

---
+++ database/dump.sql
@@ -0,0 +1,93 @@
+-- vim: ft=mysql
+-- CREATE USER
+CREATE USER IF NOT EXISTS '205CDE'@'localhost' IDENTIFIED BY 'EDC502';
+-- GRANT Permission 
+GRANT ALL ON `205CDE_Project`.* TO '205CDE'@'localhost';
+-- GRANT ALL ON `database`.* TO 'user'@'localhost' IDENTIFIED BY 'password';
+
+-- Create DB
+CREATE DATABASE IF NOT EXISTS 205CDE_Project;
+
+-- MySQL dump 10.16  Distrib 10.1.22-MariaDB, for osx10.11 (x86_64)
+--
+-- Host: localhost    Database: 205CDE_Project
+-- ------------------------------------------------------
+-- Server version	10.1.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `navigation`
+--
+
+DROP TABLE IF EXISTS `navigation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `navigation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `caption` varchar(64) NOT NULL,
+  `href` varchar(64) NOT NULL,
+  `bar` enum('shared','cms') NOT NULL DEFAULT 'shared',
+  `position` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `navigation`
+--
+
+LOCK TABLES `navigation` WRITE;
+/*!40000 ALTER TABLE `navigation` DISABLE KEYS */;
+INSERT INTO `navigation` VALUES (3,'Home','.','shared',0),(4,'Sales Order','#','shared',1),(5,'Shared Navbar','shared_navbar.html','cms',0),(6,'Staff','staff.html','cms',2),(7,'Service Order','#','shared',2),(8,'CMS Navbar','cms_navbar.html','cms',1);
+/*!40000 ALTER TABLE `navigation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `staff`
+--
+
+DROP TABLE IF EXISTS `staff`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `staff` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `role` enum('user','manager','admin','') NOT NULL DEFAULT 'user',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `staff`
+--
+
+LOCK TABLES `staff` WRITE;
+/*!40000 ALTER TABLE `staff` DISABLE KEYS */;
+INSERT INTO `staff` VALUES (1,'admin','admin','Leo','Sin','lfsin3-c@my.cityu.edu.hk','admin'),(2,'user','user','Abc','Def','test@abc.com','user');
+/*!40000 ALTER TABLE `staff` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-03-12 15:29:42

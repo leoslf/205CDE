861ae955f0c3be43e011e25180632d42acba9e2c
Leo Sin <leo69219362@gmail.com>
2018-03-13 13:43:07 +0800

    update navbar template

--- crm/__init__.py
+++ crm/__init__.py
@@ -10,7 +10,7 @@ app.config.from_object(DevelopmentConfig)
 app.register_blueprint(admin, url_prefix="/admin")
 app.secret_key = "KEY"
 # enable these functions in templates
-app.jinja_env.globals.update(zip=zip, list=list, query=query)
+app.jinja_env.globals.update(zip=zip, list=list, str=str, query=query)
 # Trim Empty Lines
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True

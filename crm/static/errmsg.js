274e5c7bdeb1e9d500ce2cc6af8c7d8acd248d36
Leo Sin <leo69219362@gmail.com>
2018-03-18 03:57:50 +0800

    errmsg is now using bootstrap alert; moved the table in all2's db into the current db, changed table name specification on page template

--- crm/static/errmsg.js
+++ crm/static/errmsg.js
@@ -1,9 +1,9 @@
 function dump_errmsg() {
     var errmsg = get_cookie("errmsg");
     set_cookie("errmsg", "", 0);
-    // console.log("errmsg: " + errmsg);
-    if (errmsg.length > 0)
-        $("#errmsg").append("<span class=\"glyphicon glyphicon-remove-sign\"></span> " + errmsg);
+    if (errmsg.length > 0) 
+        $("#errmsg").removeClass("hidden").append("<span class=\"glyphicon glyphicon-remove-sign\"></span> " + errmsg);
+    
     console.log(errmsg);
 }
 
